e17dca4a7dd44e82c14c7ee53699cd1040fe155c
KaterynaChernetska <vesnavved@gmail.com>
2023-03-19 17:56:30 +0200

    Add new version of video.js

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -32,7 +32,7 @@ export const Video = (props) => {
     const player = playerRef.current;
 
     return () => {
-      if (player) {
+      if (player && !player.isDisposed()) {
         player.dispose();
         playerRef.current = null;
       }
